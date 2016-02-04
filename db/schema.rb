12a1c51b5ec71fe98582b8bcd68229d9f4b9b0d2
Axel Rigaud <axel.rigaud@jobteaser.com>
2016-02-04 19:44:33 +0100

    add name to places

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160204100207) do
+ActiveRecord::Schema.define(version: 20160204184231) do
 
   create_table "places", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160204100207) do
     t.string   "youtube_link"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "name"
   end
 
 end
