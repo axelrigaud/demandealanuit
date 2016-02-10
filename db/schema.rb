518b98c791fc945865a6c1363554ddb884747eb2
Axel Rigaud <axel.rigaud@jobteaser.com>
2016-02-10 16:23:44 +0100

    now using postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160204184231) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "places", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
