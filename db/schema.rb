01fb0e00b6b08e73e0d534920278035306f7df80
axel rigaud <axel.rigaud@gmail.com>
2016-02-23 20:49:13 +0100

    no names anymore

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20160223193821) do
     t.string   "youtube_link"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.string   "name"
     t.string   "tagline"
     t.integer  "xpos"
     t.integer  "ypos"
