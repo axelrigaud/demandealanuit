c89dad11bc16328c9ec0344f4bda2a82ce10915e
axel rigaud <axel.rigaud@gmail.com>
2016-02-23 20:56:42 +0100

    places seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Place.create(title: 'alexanderplatz', 
+			 description: 'Je dépense mes dernières pièces dans un tour de grande roue. Berlin, réputée capitale
+culturelle, a provoqué en moi un oubli de la culture au profit d’un plaisir immédiat et primaire. Je pense à tous les afters qui se poursuivent à cette heure avancée de l’après-midi et où il y a forcément quelqu’un qui drope Blue Monday de New Order pour paraître dans le coup. J’aimerais provoquer la ville en duel, lui crier de ces choses que l’on peut dire à d’autres capitales – le « À nous deux ! » de Rastignac – mais il n’y a à Berlin, je le sais à présent, rien d’autre à conquérir que soi-même. ', 
+			 youtube_link: 'V5s-KLGVcTI&t=1663s', 
+			 tagline: 'page 214 : tu traînes sur Alexanderplatz', 
+			 xpos: 50, 
+			 ypos: 50)
+
+Place.create(title: 'ritter', 
+			 description: 'Il me tire les cheveux afin que mon cou s’offre à ses baisers furieux. Je veux qu’encore il me repeigne. « Empêche-moi de faire quoique ce soit, s’il te plaît. Par respect pour elle. Je ne veux pas détruire ce que j’ai construit. » Je suis l’éternelle vis en trop des amours Ikea, moi qui ne construis rien, qui prends les choses comme elles viennent. Pour le punir, je lui offre une ligne de coke bien trop grosse. Au loin, Solomun lance le sidérurgique 5 After 909 de Justus Köhncke.', 
+			 youtube_link: 'V5s-KLGVcTI&t=1663s', 
+			 tagline: 'page 101 : tu t’égares au Ritter Butzke', 
+			 xpos: 50, 
+			 ypos: 50)
+
+Place.create(title: 'golden', 
+			 description: 'Le club est surnommé Die Apotheke – la pharmacie – car tout s’y achète, à toute heure. Il ne s’agit d’aller nulle part, seulement d’être. Chacun danse pour soi. C’est ce que Berlin a de plus précieux, ce bel équilibre entre complet anonymat et fête de famille. Déjà, j’ai appris à sortir sans but, sans autre fin que la dépense énergétique. « Speed ou trip ? », scande inlassablement un dealer de cinquante piges, ancien acrobate de cirque bulgare. Je fais signe au DJ, lui indique mon oreille et la platine : il me montre la pochette du vinyle, pointe le remix d’Extrawelt de The Leopard. ', 
+			 youtube_link: 'V5s-KLGVcTI&t=1663s', 
+			 tagline: 'page 75 : tu accueilles le jour au Golden Gate', 
+			 xpos: 50, 
+			 ypos: 50)
