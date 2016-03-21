f7f767d6888a0bb72cc14ad8821d05bc27fa751e
axel rigaud <axel.rigaud@gmail.com>
2016-03-21 21:33:33 +0100

    better jquery

--- app/assets/javascripts/loader.js
+++ app/assets/javascripts/loader.js
@@ -1,19 +1,17 @@
-// $('#welcomeModal').modal({ show: false})
-
 $( window ).load(function() {
   var overlay = $('.md-overlay');
   
   $('.loader-wrapper').fadeOut(
       "slow",
       function() {
-        var welcome_modal = $('#welcome')
-        var close = welcome_modal.find('.md-close');
-        welcome_modal.addClass('md-show');
+        var $welcome_modal = $('#welcome')
+        var close = $welcome_modal.find('.md-close');
+        $welcome_modal.addClass('md-show');
         overlay.click(function(){
-          welcome_modal.removeClass('md-show');
+          $welcome_modal.removeClass('md-show');
         });
         close.click(function(){
-          welcome_modal.removeClass('md-show');
+          $welcome_modal.removeClass('md-show');
         });
       }
     );
--- app/assets/javascripts/modals.js
+++ app/assets/javascripts/modals.js
@@ -1,10 +1,19 @@
 $(document).ready(function(){
 
+  function hideModal($target) {
+    $target.removeClass('md-show');
+  }
+
   $('.header__anchor').click(function(e){
     e.preventDefault();
-    $('#about-the-book').addClass('md-show');
+    var $about_the_book = $('#about-the-book');
+    var close = $about_the_book.find('.md-close');
+    $about_the_book.addClass('md-show');
+    close.click(function(){
+      hideModal($about_the_book);
+    });
     overlay.click(function(){
-      $('#about-the-book').removeClass('md-show');
+      hideModal($about_the_book);
     });
   })
 
@@ -15,21 +24,22 @@ $(document).ready(function(){
     $(this).click(function(e){
       e.preventDefault();
       var target = $(this).data('target');
+      var $target = $(target)
       var close = $(target).find('.md-close');
-      $(target).addClass('md-show');
+      $target.addClass('md-show');
 
       if (mobileAndTabletcheck()) {
-        var play_link = $(target).find('.play-track-modal')
+        var play_link = $target.find('.play-track-modal')
         var video_id = play_link.data('video-id');
         play_link.css({'display':'none'});
         $('<iframe width="100%" height="100px" src="https://www.youtube.com/embed/'+video_id+'" frameborder="0" allowfullscreen></iframe>').insertBefore(close)
       }
 
       overlay.click(function(){
-        $(target).removeClass('md-show');
+        hideModal($target);
       })
       close.click(function(){
-        $(target).removeClass('md-show');
+        hideModal($target);
       })
     })
   })
