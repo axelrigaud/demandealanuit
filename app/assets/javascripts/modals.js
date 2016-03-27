7866f7812910fffa9a489d7454c0e019e10d4b08
axel rigaud <axel.rigaud@gmail.com>
2016-03-27 18:49:22 +0200

    gfx and youtube fixes

--- app/assets/javascripts/modals.js
+++ app/assets/javascripts/modals.js
@@ -35,7 +35,8 @@ $(document).ready(function(){
         var play_link = $target.find('.play-track-modal')
         var video_id = play_link.data('video-id');
         play_link.css({'display':'none'});
-        //$('<iframe width="100%" height="100px" src="https://www.youtube.com/embed/'+video_id+'" frameborder="0" allowfullscreen></iframe>').insertBefore(close)
+        if (!$target.find('iframe').length) // no iframe append yet
+          $('<iframe width="100%" height="100px" src="https://www.youtube.com/embed/'+video_id+'" frameborder="0" allowfullscreen></iframe>').insertBefore(close)
       }
 
       overlay.click(function(){
