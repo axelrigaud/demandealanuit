0382a32500b3d2f595e68000cda3af705d185cdc
axel rigaud <axel.rigaud@gmail.com>
2016-05-10 22:01:54 +0200

    carousel working

--- app/assets/javascripts/frontend.js
+++ app/assets/javascripts/frontend.js
@@ -16,4 +16,14 @@
 //= require bootstrap-sprockets
 //= require modals
 //= require loader
-
+//= require owl.carousel/dist/owl.carousel.js
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    items:1,
+    margin:1,
+    loop:true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplaySpeed: 1000
+  });
+});
