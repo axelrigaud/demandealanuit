2ff3d8dce57f40cba2acbf2373bd5ba10474170c
axel rigaud <axel.rigaud@gmail.com>
2016-10-13 10:26:42 +0200

    better js for loader

--- app/assets/javascripts/loader.js
+++ app/assets/javascripts/loader.js
@@ -1,24 +1,22 @@
+var places = $('.place-link');
+var overlay = $('.md-overlay');
+
+function revealIcons() {
+  for (var i = 0; i < places.length; i++) {
+    scheduleReveal(places[i],i);
+  }
+}
+
+function scheduleReveal(icon,index) {
+  var offset = index * 150;
+  setTimeout(function(){revealIcon(icon)},offset);
+}
+
+function revealIcon(icon) {
+  $(icon).addClass('reveal');
+}
+
 $( window ).load(function() {
-  var overlay = $('.md-overlay');
-  
   $('.loader-wrapper').fadeOut(
-      "slow",revealIcons);
-
-   var places = $('.place-link')
-
-  function revealIcons () {
-    console.log(places)
-    for (var i = 0; i < places.length; i++) {
-      scheduleReveal(places[i],i);
-    }
-  }
-
-  function scheduleReveal (icon,index) {
-    var offset = index * 150;
-    setTimeout(function(){revealIcon(icon)},offset);
-  }
-
-  function revealIcon (icon) {
-    $(icon).addClass('reveal');
-  }
-})
+      'slow',revealIcons);
+});
